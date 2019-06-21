2534257d9fc1ea28b25f436a04f7ed979829c479
unknown <msdsupp@gitl.com>
2019-06-21 17:28:46 +0530

    from to date display complete

--- admin.php
+++ admin.php
@@ -30,9 +30,9 @@
     <a href="entry.php">Add L+1/L-1</a>
     <h2>Get Details</h2>
         <label >Date (From)</label> 
-        <input type="date" name="date">
+        <input type="date" name="date" value="<?php echo isset($_POST['date']) ? $_POST['date'] : '' ?>">
         <label >Date (To)</label> 
-        <input type="date" name="todate">
+        <input type="date" name="todate" value="<?php echo isset($_POST['todate']) ? $_POST['todate'] : '' ?>">
         <input type="submit" name="show_details">
 	</form> <br>
 </body>
@@ -48,29 +48,76 @@
 		} 
         mysqli_select_db($con,'harshi');
         $date = $_POST["date"];
-        $date = $_POST["todate"];
-		$select = "select * from lminus as A left join (select * from details where wdate='$date') as B on A.codeplus1=B.codeLplus1 and A.codeminus1=B.codeminus left join user on user.username = codeplus1 order by A.codeplus1, A.codeminus1";
-		$status = mysqli_query($con,$select);
-		if(!$status){
+        $start = $date;
+        $todate = $_POST["todate"];
+        $sql = "select wdate from details where wdate >= '$date' and wdate <= '$todate' group by wdate";
+        $st = mysqli_query($con,$sql);
+        if(!$st){
 		    die("Unable to load data.".mysqli_error($con));
         }
-        $d = date("l, F d, Y", strtotime($date));
-        echo "<table><tr><th>Code</th><th>L+1/data given by</th><th>Name</th><th>DomainFunction</th><th>Dept</th><th>Project</th><th>".$d."</th></tr>";
+        $tableCount = 0;
+        while($dr = mysqli_fetch_array($st,MYSQLI_NUM)){
+            $date = $dr[0];
+            $select = "select * from lminus as A left join (select * from details where wdate='$date') as B on A.codeplus1=B.codeLplus1 and A.codeminus1=B.codeminus left join user on user.username = codeplus1 order by A.codeplus1, A.codeminus1";
+            $status = mysqli_query($con,$select);
+            if(!$status){
+                die("Unable to load data.".mysqli_error($con));
+            }
+            $d = date("l, F d, Y", strtotime($date));
+            if($tableCount==0){
+                echo "<table class='hitable'><tr><th>Code</th><th>L+1/data given by</th><th>Name</th><th>DomainFunction</th><th>Dept</th><th>Project</th><th>".$d."</th></tr>";
+                $rowCount = 0; 
+                while($row = mysqli_fetch_array($status,MYSQLI_NUM)){
+                    $rowCount++;
+                    echo "<tr><td>".$row[0]."</td>";
+                    echo "<td>".$row[14]."</td>";
+                    echo "<td>".$row[2]."</td>";
+                    echo "<td>".$row[3]."</td>";
+                    echo "<td>".$row[4]."</td>";
+                    echo "<td>".$row[9]."</td>";
+                    if($row[11]==""){
+                        echo "<td style='background-color:red;'>--</td></tr>"; 
+                    }
+                    else{
+                        echo "<td>".$row[11]." %</td></tr>"; 
+                    }
+                }
+                echo "</table>";
+            }
+            else{
+                echo "<table class='hitable'><tr><th>".$d."</th></tr>";
+                $rowCount = 0; 
+                while($row = mysqli_fetch_array($status,MYSQLI_NUM)){
+                    $rowCount++;
+                    echo "<tr>";
+                    if($row[11]==""){
+                        echo "<td style='background-color:red;'>--</td></tr>"; 
+                    }
+                    else{
+                        echo "<td>".$row[11]." %</td></tr>"; 
+                    }
+                }
+                echo "</table>";
+            }
+            $tableCount++;
+        }
+        $select = "select avg(utilization) from lminus as A left join (select * from details where wdate>='$start' and wdate<='$todate') as B on A.codeplus1=B.codeLplus1 and A.codeminus1=B.codeminus  
+        group by codeLplus1, codeminus
+        ORDER BY `A`.`codeplus1` ASC, `A`.`codeminus1` ASC";
+        $status = mysqli_query($con,$select);
+        if(!$status){
+            die("Unable to load data.".mysqli_error($con));
+        }
+        echo "<table class='hitable'><tr><th>Average utilization</th></tr>";
         $rowCount = 0; 
         while($row = mysqli_fetch_array($status,MYSQLI_NUM)){
             $rowCount++;
-            echo "<tr><td>".$row[0]."</td>";
-            echo "<td>".$row[14]."</td>";
-            echo "<td>".$row[2]."</td>";
-            echo "<td>".$row[3]."</td>";
-            echo "<td>".$row[4]."</td>";
-            echo "<td>".$row[9]."</td>";
-            
-            if($row[11]==""){
-                echo "<td style='background-color:red;'>".$row[11]."</td></tr>"; 
+            echo "<tr>";
+            if($row[0]==""){
+                echo "<td style='background-color:red;'>--</td></tr>"; 
             }
             else{
-                echo "<td>".$row[11]."</td></tr>"; 
+                echo "<td>".$row[0]." %</td></tr>"; 
             }
         }
         echo "</table>";
