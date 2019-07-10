1fd81e3c7f823f4dacc3cb0703374b18831c12ba
Harshita Agrawal <msdsupp@gitl.com>
2019-07-10 14:03:18 +0530

    Date picker added, precautions for monday date, previous week date added

--- index.php
+++ index.php
@@ -37,17 +37,50 @@
 	<link type="text/css" rel="stylesheet" href="main.css" />
 </head>
 <body>	
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+<script>
+    function EnableMonday(date) {
+        var day = date.getDay();  
+        // If day == 1 then it is Monday
+        if (day == 1) {
+            return [true] ; 
+        } 
+        else { 
+            return [false] ;
+        }
+    }
+    $(function() {
+        $( "#datepicker" ).datepicker({
+            beforeShowDay: EnableMonday,
+            dateFormat: ' d MM, yy' ,
+            showWeek: true,
+            firstDay: 1,
+            minDate: '-6d'
+        });
+    });
+</script>
+<style>
+#datepicker{
+    border: 1px solid teal;
+    color: teal;
+}
+.userSideForm{
+    margin-left:30%;
+}
+</style>
     <ul>
         <li class="li"><a class="active" href="index.php">Home</a></li>
         <li class="li"><a href="admin.php">Admin</a></li>
         <li class="li" style="float:right"><a href="logout.php">Logout</a></li>
         <li class="li" style="float:right"><a ><span>You are logged in as: <?php echo $_SESSION['nam'];?></span> </a></li>
     </ul>  <br><br><br> <img src="logo.png" width="15%"><br>
-	<form method="post" class="userSideForm">
-		<h1>Enter current week Monday's date:</h1>
-        <label >Date</label> 
-        <input type="date" name="date">
-        <input type="submit" name="submit" value="Submit and next" class="btn">
+	<form method="post" class="userSideForm"  style="border-radius:5px; width:40%" autocomplete="off">
+		<h1 style="font-size:15px">Select starting date of week for which you want to forecast utilization of your Ls.:</h1>
+        <label style="font-size:15px" >Date: </label>
+        <input name="date" style="width:100%;" id="datepicker" readonly="readonly"> <br> 
+        <input type="submit" name="submit" value="Next" class="btn" style="font-size:15px">
         <div id="error_msg"><?php echo $msg ; ?></div>
 	</form> 
 </body>
